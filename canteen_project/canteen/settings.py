7e9efc7d7a8e874b2a42a084363e21ae254e67c4
jithugaming79-art <jithugaming79art@gmail.com>
2026-02-25 20:10:45 +0530

    TEMP: DEBUG=True to see place_order traceback

--- canteen_project/canteen/settings.py
+++ canteen_project/canteen/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True  # TEMPORARY: see traceback
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
