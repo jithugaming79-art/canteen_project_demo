0819209414f907493206c9570e0fe87b0cc6057d
jithugaming79-art <jithugaming79art@gmail.com>
2026-02-25 19:31:34 +0530

    TEMP: Enable DEBUG to diagnose 500 error

--- canteen_project/canteen/settings.py
+++ canteen_project/canteen/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True  # TEMPORARY: Force DEBUG to see error traceback on Render
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
